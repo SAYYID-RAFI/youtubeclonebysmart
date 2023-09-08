cc49e1a9fe53d9ef1169e6a55096a2c28fd98625
SAYYID-RAFI <144410390+SAYYID-RAFI@users.noreply.github.com>
2023-09-08 11:20:20 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,8 @@
+ var menuicon = document.querySelector(".menu-icon");
+ var sidebar = document.querySelector(".sidebar");
+
+ menuicon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    
+    
+ }
